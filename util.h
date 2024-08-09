53e489dfe7055b1ec5ccafa2ee9156411cf59649
Mohamed labbit <151045562+Mohammed0101-lgtm@users.noreply.github.com>
2024-08-09 03:54:18 +0100

    Add files via upload

---
+++ util.h
@@ -0,0 +1,47 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdlib.h>
+
+typedef struct Node {
+    int val;
+    struct Node *next;
+}Node;
+
+char *get_string(const char *__restrict prompt);
+
+int  *get_array(size_t size);
+
+int min(int _count, ...);
+
+int max(int _count, ...);
+
+float float_min(int _count, ...);
+
+float float_max(int _count, ...);
+
+double double_min(int _count, ...);
+
+double double_max(int _count, ...);
+
+unsigned long factorial(int n);
+
+void print_row(int *row, size_t size);
+
+void print_matrix(int **mat, size_t size) ;
+
+void append_to_array(int **arr, size_t *__size, int n);
+
+void insert_to_array(int **arr, size_t *__size, int pos, int val);
+
+void remove_from_array(int **arr, size_t *__size, int pos);
+
+void append_to_list(struct Node *head, int val);
+
+void remove_from_list(struct Node **head, int val);
+
+int binary_search(int *arr, int val, int low, int high);
+
+
+#endif // UTIL_H
+
